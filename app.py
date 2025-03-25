4bbe016d487465a87b802a67190269fbed3e11e8
victor-gauthier4555 <144247004+victor-gauthier4555@users.noreply.github.com>
2025-03-25 16:51:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
 import os
-import requests
+
 app = Flask(__name__)
 CORS(app)  # Autoriser les requêtes depuis React
 
-RECAPTCHA_SECRET_KEY = "6LdAJvMqAAAAAKP1relcls94Fr91l9xNWlqOa4FZ"
-
 # Fichier pour stocker les compteurs
 DATA_FILE = "compteurs.json"
-
-def verify_recaptcha(token):
-    """ Vérifie le jeton reCAPTCHA avec Google """
-    url = "https://www.google.com/recaptcha/api/siteverify"
-    payload = {"secret": RECAPTCHA_SECRET_KEY, "response": token}
-    response = requests.post(url, data=payload)
-    result = response.json()
-    print("Google reCAPTCHA Response:", result)
-    return result.get("success", False) and result.get("score", 0) > 0.5  # Seuil anti-bot
 
 def load_counters():
     if os.path.exists(DATA_FILE):
@@ -33,6 +22,7 @@ def save_counters(tests_commences, tests_finies):
 
 # Charger les compteurs au démarrage du serveur
 tests_commences, tests_finies = load_counters()
+
 @app.route('/')
 def home():
     return "Flask fonctionne avec ngrok !"
@@ -42,10 +32,11 @@ def start_test():
     global tests_commences
     data = request.get_json()
     print("Données reçues par Flask :", data)
-    token = data.get("token", "")
 
-    if not verify_recaptcha(token):
-        return jsonify({"success": False, "message": "Captcha invalide"}), 403
+    # Protection anti-bot simple (champ honeypot)
+    honeypot_value = data.get("antiBot", "")
+    if honeypot_value:
+        return jsonify({"success": False, "message": "Bot détecté (honeypot)"}), 403
 
     tests_commences += 1
     return jsonify({"success": True, "tests_commences": tests_commences})
@@ -63,6 +54,7 @@ def get_stats():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
